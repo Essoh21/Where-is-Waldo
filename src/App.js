6e5d5c0a242b6e3c1d51c1d8258d322318c8fdc1
essohanam <essohanam.alou@imsp-uac.org>
2023-04-03 22:52:05 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React from 'react';
+import StyledComponent from './StyledComponent';
+function App() {
+  return (
+
+    <StyledComponent>
+      Hello World!
+    </StyledComponent>
+  );
+
+}
+
+export default App;
